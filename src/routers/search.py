7fd2f564f8bc6e42f939e441acca913e6e8c062c
Nairen <51224119+InternSG@users.noreply.github.com>
2020-05-29 18:41:10 +0800

    Added docstring

--- src/routers/search.py
+++ src/routers/search.py
@@ -3,5 +3,13 @@ from fastapi import APIRouter, HTTPException
 router = APIRouter()
 
 @router.get('/')
-async def search(artist_name: str = ''):
+async def search(artist_name: str):
+    """
+    Basic search endpoint
+
+    :param artist_name: name of artist to be searched
+    :type artist_name: str
+    :return: artist_name
+    :rtype: JSON
+    """
     return artist_name
